594adaedfec8f08baa3f6bf2bf529d15ee71fb20
Ian Torquato <iantorquato2@gmail.com>
2021-08-04 20:42:36 -0300

    Challenge Finalized 🎉

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -7,5 +7,20 @@ interface ModalViewImageProps {
 }
 
 export function ModalViewImage({ isOpen, onClose, imgUrl }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent alignSelf="center" bgColor="pGray.800">
+        <ModalBody p={2}>
+          <Image src={imgUrl} />
+        </ModalBody>
+
+        <ModalFooter justifyContent="start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
+import Head from 'next/head';
 
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
@@ -8,36 +9,65 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Card = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+type IInfiniteQueryResponse = {
+  after: number | null;
+  data: Card[];
+};
+
 export default function Home(): JSX.Element {
-  const {
-    data,
-    isLoading,
-    isError,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  const getImages = async ({ pageParam = null }): Promise<IInfiniteQueryResponse> => {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  };
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
-  }, [data]);
+  const { data, isLoading, isError, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<
+    unknown,
+    unknown,
+    IInfiniteQueryResponse
+  >('images', getImages, {
+    getNextPageParam: (lastPage: { after: number }) => lastPage.after,
+  });
 
-  // TODO RENDER LOADING SCREEN
+  const formattedData = useMemo(() => data?.pages.map(page => page.data).flat(), [data]);
 
-  // TODO RENDER ERROR SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
+      <Head>
+        <title>UpFi</title>
+
+        <link rel="shortcut icon" href="logo.svg" />
+      </Head>
+
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} mt="8">
+            {isFetchingNextPage ? 'Carregando' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
